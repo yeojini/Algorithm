d221f75afea79f209453831da064eb676d7da9c3
Yeojin Choi <who3637@naver.com>
2020-03-15 21:20:58 +0900

    200315 완전탐색/오차/소수점

--- SWEA/No1245_균형점.cpp
+++ SWEA/No1245_균형점.cpp
@@ -1,3 +1,13 @@
+/*
+# 1. 풀이
+
+- 입력받은 모든 x 좌표들 중 연속한 x 좌표를 골라 mid 좌표를 설정한다.
+- mid 좌표를 기준으로 왼쪽 인력, 오른쪽 인력을 구한다
+- 왼쪽 인력이 더 크면 mid 좌표를 왼쪽 좌표로, 오른쪽 인력이 더 크면 mid 좌표를 오른쪽 좌표로하여 다시 while 수행
+- 오른쪽 좌표와 왼쪽 좌표의 오차가 1e-12 이면 break; 하고 result 배열에 값 저장
+
+*/
+
 #include <string>
 #include <algorithm>
 #include <iostream>
